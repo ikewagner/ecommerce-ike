37ce0e2af140ff905598d2e7f5435d7496c15d02
Henrique wagner <ike@MacBook-Pro-de-Henrique.local>
2022-03-07 21:07:01 -0300

    Ajustes finais

--- pages/api/checkout_sessions/index.js
+++ pages/api/checkout_sessions/index.js
@@ -6,20 +6,11 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const session = await stripe.checkout.sessions.create({
-        phone_number_collection: {
-            enabled: true,
-          },
         mode: 'payment',
         payment_method_types: ['card'],
-        line_items: req?.body?.items ?? [
-            {
-                price: '{{PRICE_ID}}',
-                quantity: 1,
-            }
-        ],
+        line_items: req?.body?.items ?? [],
         success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/cart`,
-        
       });
 
       res.status(200).json(session);
